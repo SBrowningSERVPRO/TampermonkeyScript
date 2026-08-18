903c193405b7711d88061bd689edabfd838ed854
SBrowningSERVPRO <sbrowning@servproctn.com>
2026-08-18 14:51:07 -0400

    Bump version from 9.0 to 9.1

--- script.user.js
+++ script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SERVPRO Office Auto-Fill
 // @namespace    http://tampermonkey.net/
-// @version      9.0
+// @version      9.1
 // @description  Auto-fill participant dropdowns based on selected SERVPRO office and estimator
 // @author       Samuel Browning (with fixes)
 // @match        https://servpro.ngsapps.net/*
